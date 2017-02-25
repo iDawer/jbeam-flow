3a938291287ec36ac69665ff433af53ac0be83c8
Dawer <iDawer@users.noreply.github.com>
2017-02-25 23:19:10 +0500

    persistent SpaceView3D draw handler, finally 'show_jbeam_nodes' moved from Mesh to Object, + force nodes display property to Scene

--- display_nodes.py
+++ display_nodes.py
@@ -9,26 +9,31 @@ from bpy_extras.view3d_utils import location_3d_to_region_2d
 
 
 def draw_ui(self, context):
-    # context.mode == 'EDIT_MESH' cuz we are in the Mesh Display panel
     if context.active_object.type == 'MESH':
         layout = self.layout
         layout.label(text="Jbeam info:")
 
         row = layout.row(align=True)
-        row.prop(context.active_object.data, 'show_jbeam_nodes', toggle=True)
+        row.prop(context.active_object, 'show_jbeam_nodes')
+
+
+def draw_ui_all(self, context):
+    layout = self.layout
+    layout.label(text="Jbeam info:")
+
+    row = layout.row(align=True)
+    row.prop(context.scene, 'show_all_jbeam_nodes')
 
 
 class KotL:  # magic here!
     draw_nodes_handle = None
-    drawing_mesh = None
 
     @staticmethod
-    def start_draw(args):
+    def start_draw(args: tuple):
         if KotL.draw_nodes_handle is not None:
             KotL.stop_it()
         KotL.draw_nodes_handle = bpy.types.SpaceView3D.draw_handler_add(
             draw_nodes, args, 'WINDOW', 'POST_PIXEL')  # PRE_VIEW POST_VIEW POST_PIXEL
-        KotL.drawing_mesh = args[0]
 
     @staticmethod
     def stop_it():
@@ -36,73 +41,67 @@ class KotL:  # magic here!
         KotL.draw_nodes_handle = None
 
 
-def update_show(self, context):
-    print('update_show', time.time())
-    if self.show_jbeam_nodes:
-        KotL.start_draw((self, context))
-    else:
-        KotL.stop_it()
+def draw_nodes():
+    # enter_time = time.time()
+    # i = 0
 
-
-def draw_nodes(self, context):
-    print('draw', time.time())
-    obj = context.active_object
-    if not obj.data.is_editmode:
-        self.show_jbeam_nodes = False
-        return
-
-    bm = bmesh.from_edit_mesh(obj.data)
-    id_layer = bm.verts.layers.string.get('jbeamNodeId')
-    if id_layer is None:
-        return
-
-    obj_location = obj.location
-    region = context.region
-    rv3d = context.region_data
-
-    font_id = 0
-    blf.size(font_id, 13, 72)
-
-    for vert in bm.verts:
-        world_co = obj_location + vert.co
-        co_2d = location_3d_to_region_2d(region, rv3d, world_co)
-        if co_2d is None:
-            # vert is behind the origin of a perspective view
+    context = bpy.context
+    for obj in context.visible_objects:
+        if obj.type != 'MESH' or not (context.scene.show_all_jbeam_nodes or obj.show_jbeam_nodes):
             continue
-        blf.position(font_id, co_2d.x, co_2d.y, 0)
-        blf.draw(font_id, vert[id_layer].decode())
 
-    blf.position(font_id, 1, 1, 0)
-    blf.draw(font_id, "Hello Word ")
+        if obj.data.is_editmode:
+            bm = bmesh.from_edit_mesh(obj.data)
+        else:
+            bm = bmesh.new()
+            bm.from_mesh(obj.data)
+        id_layer = bm.verts.layers.string.get('jbeamNodeId')
+        if id_layer is None:
+            continue
 
-    # 50% alpha, 2 pixel width line
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glColor4f(0.0, 0.0, 0.0, 0.5)
-    bgl.glLineWidth(2)
+        obj_matrix = obj.matrix_world
+        region = context.region
+        rv3d = context.region_data
 
-    bgl.glBegin(bgl.GL_LINE_STRIP)
-    # for x, y in self.mouse_path:
-    #     bgl.glVertex2i(x, y)
+        font_id = 0
+        bgl.glColor3f(1.0, 1.0, 1.0)
+        blf.size(font_id, 13, 72)
 
-    bgl.glEnd()
+        for vert in bm.verts:
+            world_co = obj_matrix * vert.co
+            co_2d = location_3d_to_region_2d(region, rv3d, world_co)
+            if co_2d is None:
+                # vert is behind the origin of a perspective view
+                continue
+            blf.position(font_id, co_2d.x + 3, co_2d.y, 0)
+            blf.draw(font_id, vert[id_layer].decode())
+            # i += 1
 
-    # restore opengl defaults
-    bgl.glLineWidth(1)
-    bgl.glDisable(bgl.GL_BLEND)
-    bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
+    pass
+    # print(i, 'nodes drawn in', time.time() - enter_time)
 
 
 def register():
-    bpy.types.Mesh.show_jbeam_nodes = BoolProperty(
+    bpy.types.Object.show_jbeam_nodes = BoolProperty(
         name="Nodes",
         description="Display jbeam nodes id",
-        update=update_show
+        options={'HIDDEN'},
+    )
+    bpy.types.Scene.show_all_jbeam_nodes = BoolProperty(
+        name="All nodes",
+        description="Force display all jbeam nodes id",
+        options={'HIDDEN'},
     )
     bpy.types.VIEW3D_PT_view3d_meshdisplay.append(draw_ui)
+    bpy.types.OBJECT_PT_display.append(draw_ui)
+    bpy.types.VIEW3D_PT_view3d_display.append(draw_ui_all)
+    KotL.start_draw(())
 
 
 def unregister():
-    if KotL.drawing_mesh is not None:
-        KotL.drawing_mesh.show_jbeam_nodes = False
-    del bpy.types.Mesh.show_jbeam_nodes
+    KotL.stop_it()
+    del bpy.types.Object.show_jbeam_nodes
+    del bpy.types.Scene.show_all_jbeam_nodes
     bpy.types.VIEW3D_PT_view3d_meshdisplay.remove(draw_ui)
+    bpy.types.OBJECT_PT_display.remove(draw_ui)
+    bpy.types.VIEW3D_PT_view3d_display.remove(draw_ui_all)
