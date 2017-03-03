72cf8a529c9450cb9e307dd73cc8aee230633d4b
Dawer <iDawer@users.noreply.github.com>
2017-03-03 23:47:37 +0500

    modal import

--- op_import_vehicle.py
+++ op_import_vehicle.py
@@ -1,4 +1,5 @@
 import os
+from collections import deque
 from os import path
 from itertools import chain
 
@@ -11,9 +12,9 @@ from bpy_extras.io_utils import ImportHelper
 from . import jbeam_utils
 
 
-class ImportJBeam(Operator, ImportHelper):
-    """Import from BeamNG's JBeam vehicle"""
-    bl_idname = "import_scene.jbeam_vehicle"  # important since its how bpy.ops.import_test.some_data is constructed
+class ImportJBeam(Operator, ImportHelper):  #
+    """Import BeamNG's JBeam vehicle"""
+    bl_idname = "import_scene.jbeam_vehicle"
     bl_label = "JBeam vehicle"
     bl_options = {'REGISTER', 'UNDO'}
 
@@ -23,33 +24,38 @@ class ImportJBeam(Operator, ImportHelper):
         maxlen=1024,
         subtype='DIR_PATH',
     )
+    _timer = None
+    files = []
+    total = 0
+
+    def modal(self, context, event):
+        if event.type in {'RIGHTMOUSE', 'ESC'}:
+            self.cancel(context)
+            return {'CANCELLED'}
+
+        if event.type == 'TIMER':
+            if self.files:
+                dir, name = self.files.popleft()
+                context.area.header_text_set(
+                    "Importing: %d/%d. Press Esc/RMB to cancel." % (self.total - len(self.files), self.total))
+                bpy.ops.import_mesh.jbeam(filepath=path.join(dir, name), filename=name)
+            else:
+                self.report({'INFO'}, "Import done (%d parts)" % self.total)
+                self.cancel(context)
+                return {'FINISHED'}
+        return {'PASS_THROUGH'}
+
+    def cancel(self, context):
+        context.area.header_text_set()
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
 
     def execute(self, context):
-        files = ((root, f) for root, dirs, files in os.walk(self.directory)
-                 for f in files if f.lower().endswith(".jbeam"))
+        self.files = deque((root, f) for root, dirs, files in os.walk(self.directory)
+                           for f in files if f.lower().endswith(".jbeam"))
+        self.total = len(self.files)
 
-        for dir, name in files:
-            bpy.ops.import_mesh.jbeam(filepath=path.join(dir, name), filename=name)
-
-        # builder = jbeam_utils.PartObjectsBuilder(text_block.name)
-        # parts_group = builder.visit(tree)
-        #
-        # # resulting parts_group and text names can be different
-        # parts_group['jbeam_textblock'] = text_block.name
-        # for obj in builder.get_all_objects():
-        #     context.scene.objects.link(obj)
-
-        return {'FINISHED'}
-
-        # @staticmethod
-        # def register():
-        #     bpy.types.INFO_MT_file_import.append(menu_func_draw)
-        #
-        # @staticmethod
-        # def unregister():
-        #     bpy.types.INFO_MT_file_import.remove(menu_func_draw)
-        #
-
-
-def menu_func_draw(self, context):
-    self.layout.operator(ImportJBeam.bl_idname)
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.5, context.window)
+        wm.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
